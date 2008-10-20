9bdee73697d6b03124785da5a1aa7a13938d24a1
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2008-10-20 22:01:57 +0000

    adding more class writer code

--- trunk/asm_bytecode/simple_examples/set4/src/org/botnode/asm/ClassWriter.java
+++ trunk/asm_bytecode/simple_examples/set4/src/org/botnode/asm/ClassWriter.java
@@ -4,16 +4,14 @@
  */
 package org.botnode.asm;
 
-import org.objectweb.asm.ClassWriter;
-
 /**
  * @author bbrown
  */
 public class ClassWriter {
-	
-	public static final int COMPUTE_FRAMES = 2;
-	
-	public static final int COMPUTE_MAXS = 1;
+
+    public static final int COMPUTE_FRAMES = 2;
+
+    public static final int COMPUTE_MAXS = 1;
 
     static final byte[] TYPE;
     
@@ -27,34 +25,48 @@ public class ClassWriter {
     static final int LONG = 5;
     static final int DOUBLE = 6;
     static final int NAME_TYPE = 12;
-    
     static final int UTF8 = 1;
     static final int TYPE_NORMAL = 13;
-    static final int TYPE_UNINIT = 14;    
+    static final int TYPE_UNINIT = 14;
     static final int TYPE_MERGED = 15;
-	
-	private int version;
-	private int access;
-	ByteVector pool;
-	
-	// Constant Pool Item for the name of the class.
-	private int name;
-	private int superName;
-	
+
+    private int version;
+
+    private int access;
+    
+    /**
+     * Index of the next item to be added in the constant pool.
+     */
+    int index;
+    
+    /**
+     * The constant pool's hash table data.
+     */
+    Item[] items;
+    
+    ByteVector pool;
+
+    // Constant Pool Item for the name of the class.
+    private int name;
+
+    private int superName;
+
     /**
      * This array contains the indexes of the constant pool items
      * that contain the internal names of these interfaces.
      */
-	private int[] interfaces;
-	
-	private int interfaceCount;
-	
+    private int[] interfaces;
+
+    private int interfaceCount = 0;
+
     int threshold;
-	
+
     private final boolean computeMaxs;
+
     private final boolean computeFrames;
+
     boolean invalidFrames;
-    
+
     // ------------------------------------------------------------------------
     // Static initializer
     // ------------------------------------------------------------------------
@@ -65,42 +77,42 @@ public class ClassWriter {
     static {
         int i;
         byte[] b = new byte[220];
-        String s = "AAAAAAAAAAAAAAAABCKLLDDDDDEEEEEEEEEEEEEEEEEEEEAAAAAAAADD"
-                + "DDDEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                + "AAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAIIIIIIIIIIIIIIIIDNOAA"
-                + "AAAAGGGGGGGHAFBFAAFFAAQPIIJJIIIIIIIIIIIIIIIIII";
+        String s = "AAAAAAAAAAAAAAAABCKLLDDDDDEEEEEEEEEEEEEEEEEEEEAAAAAAAADD" + "DDDEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                + "AAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAIIIIIIIIIIIIIIIIDNOAA" + "AAAAGGGGGGGHAFBFAAFFAAQPIIJJIIIIIIIIIIIIIIIIII";
         for (i = 0; i < b.length; ++i) {
             b[i] = (byte) (s.charAt(i) - 'A');
         }
         TYPE = b;
-       
+
     }
 
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    
+
     public ClassWriter(final int flags) {
         index = 1;
         pool = new ByteVector();
         items = new Item[256];
-        threshold = (int) (0.75d * items.length);                
+        threshold = (int) (0.75d * items.length);
         this.computeMaxs = (flags & COMPUTE_MAXS) != 0;
         this.computeFrames = (flags & COMPUTE_FRAMES) != 0;
     }
-    
-	public byte[] toByteArray() {
-		 
-		 ByteVector out = new ByteVector(size);		 
-		 out.putInt(0xCAFEBABE).putInt(version);
-		 out.putShort(index).putByteArray(pool.data, 0, pool.length);
-		 out.putShort(access).putShort(name).putShort(superName);
-		 out.putShort(interfaceCount);
-		 
-		 for (int i = 0; i < interfaceCount; ++i) {
-	            out.putShort(interfaces[i]);
-		 }
-		 
-	 } // End of the Method
-	
+
+    public byte[] toByteArray() {
+
+        int size = 24 + (2 * interfaceCount);
+        ByteVector out = new ByteVector(size);
+        out.putInt(0xCAFEBABE).putInt(version);
+        out.putShort(index).putByteArray(pool.data, 0, pool.length);
+        out.putShort(access).putShort(name).putShort(superName);
+        out.putShort(interfaceCount);
+
+        for (int i = 0; i < interfaceCount; ++i) {
+            out.putShort(interfaces[i]);
+        }
+        
+        return out.data;
+    } // End of the Method
+
 }
