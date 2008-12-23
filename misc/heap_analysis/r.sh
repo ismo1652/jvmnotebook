6847f30e9b0f49744315ce0a89a41e7bf33c53ad
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2008-12-23 03:05:51 +0000

    more changes, add some code improvements

--- misc/heap_analysis/r.sh
+++ misc/heap_analysis/r.sh
@@ -5,7 +5,7 @@ javac CrashJVM.java
 FORMAT=,format=b
 
 # Add profiling and verbose garbage collection
-java -verbosegc -Xrunhprof:file=dump.txt${FORMAT} CrashJVM -Xms24MB -Xmx32MB
+java -verbosegc -Xrunhprof:file=dump.txt${FORMAT} Main -Xms24MB -Xmx32MB > d.txt
 
 # To run the server:
 jhat dump.txt
