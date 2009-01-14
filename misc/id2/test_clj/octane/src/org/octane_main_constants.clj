3832dc8abb7e05b4ad2924497288604e5604f7cc
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2009-01-14 22:47:33 +0000

    splitting out files

---
+++ misc/id2/test_clj/octane/src/org/octane_main_constants.clj
@@ -0,0 +1,61 @@
+;;; -------------------------------------------------------
+;;; Copyright (c) Berlin Brown:. All rights reserved.
+;;;
+;;; Copyright (c) 2006-2007, Botnode.com, Berlin Brown
+;;; http://www.opensource.org/licenses/bsd-license.php
+
+;;; All rights reserved.
+
+;;; Redistribution and use in source and binary forms, with or without modification,
+;;; are permitted provided that the following conditions are met:
+
+;;;    * Redistributions of source code must retain the above copyright notice,
+;;;    this list of conditions and the following disclaimer.
+;;;    * Redistributions in binary form must reproduce the above copyright notice,
+;;;    this list of conditions and the following disclaimer in the documentation
+;;;    and/or other materials provided with the distribution.
+;;;    * Neither the name of the Botnode.com (Berlin Brown) nor
+;;;    the names of its contributors may be used to endorse or promote
+;;;    products derived from this software without specific prior written permission.
+
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;; A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+;;; CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;;; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+;;;
+;;; Date:  1/5/2009
+;;; Description:
+;;;     Simple 'Find' keyword in File with SWT and Clojure
+;;; Contact:  Berlin Brown <berlin dot brown at gmail.com>
+;;; Usage:   java -cp $CP clojure.lang.Repl src/octane_main.clj
+;;;          Enter a search term and then open a file, if the term
+;;;          is found on the line then the line will be higlighted.
+
+;;; Key Functions: simple-swt create-file-menu
+;;; -------------------------------------------------------
+
+(in-ns 'org.octane)
+
+(import '(org.eclipse.swt.graphics Color RGB))
+
+(def win-size-width    880)
+(def win-size-height   740)
+
+(def orange-sel-color (new RGB 250 209 132))
+(def lightgrey-color  (new RGB 100 100 100))
+
+(def tab-1-title      "Main Buffer View")
+
+;; Hard code the style to avoid calling bitwise operator
+;; SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL
+(def swt-text-style  2818)
+
+;;; End of Script
