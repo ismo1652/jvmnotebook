d468315eeee8a782ebf19e2476dc5d3784fc3394
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2009-04-14 02:28:00 +0000

    able to get compilable code

--- scala/geneticadder/src/java/org/adder/Population.java
+++ scala/geneticadder/src/java/org/adder/Population.java
@@ -1,242 +1,274 @@
 package org.adder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Population {
-	public const int kLength = 20;
-	int nCrossover = kLength/2;
-	const int kInitialPopulation = 500;
-	const int kPopulationLimit = 500;
-	const int kMin = 0;
-	const int kMax = 8;
-	const double  kMutationFrequency = 0.33f;
-	const double  kDeathFitness = 0.00f;
-	const double  kReproductionFitness = 0.0f;
 
+	public static final int kLength = 20;
+	public static final int kInitialPopulation = 500;
+	public static final int kPopulationLimit = 500;
+	public static final int kMin = 0;
+	public static final int kMax = 8;
+	public static final double kMutationFrequency = 0.33f;
+	public static final double kDeathFitness = 0.00f;
+	public static final double kReproductionFitness = 0.0f;
 
-	ArrayList Scores = new ArrayList();
-	ArrayList Genomes = new ArrayList();
-	ArrayList GenomeReproducers  = new ArrayList();
-	ArrayList GenomeResults = new ArrayList();
-	ArrayList GenomeFamily = new ArrayList();
+	private int nCrossover = kLength / 2;
 
-	int		  CurrentPopulation = kInitialPopulation;
-	int		  Generation = 1;
-	bool	  Best2 = true;
-	
-	private void Mutate(Genome aGene)
-	{
-		if (EquationGenome.TheSeed.Next(100) < (int)(kMutationFrequency * 100.0))
-		{
-		  	aGene.Mutate();
+	private List scores = new ArrayList();
+	private List genomes = new ArrayList();
+	private List genomeReproducers = new ArrayList();
+	private List genomeResults = new ArrayList();
+	private List genomeFamily = new ArrayList();
+
+	private int currentPopulation = kInitialPopulation;
+	private int generation = 1;
+	private boolean best2 = true;
+
+	public Population() {
+		
+		for (int i = 0; i < kInitialPopulation; i++) {
+			
+			final EquationGenome aGenome = new EquationGenome(kLength, kMin, kMax);
+			int nCrossOver = EquationGenome.nextInt(EquationGenome.TheSeed, 1, (int) aGenome.length);
+			aGenome.setCrossoverPoint(nCrossover);
+			aGenome.calculateFitness();
+			genomes.add(aGenome);
+			
+		}
+
+	}
+
+	private void mutate(final Genome aGene) {
+		
+		if (EquationGenome.TheSeed.nextInt(100) < (int) (kMutationFrequency * 100.0)) {
+			aGene.mutate();
 		}
 	}
 
-	public void NextGeneration()
-	{
+	public static void removeRange(final List elements, final int startIndex, final int endIndex) {
+		int index;
+		if (startIndex > endIndex) {
+			throw new IllegalArgumentException();
+		}
+		
+		for (index = endIndex; index >= startIndex; index--) {
+			elements.remove(index);
+		}
+	}
+	
+	public static final List clone(final List list) {
+		
+		final List newList = new ArrayList();
+		for (final Iterator it = list.iterator(); it.hasNext(); ) {
+			newList.add(it.next());
+		}
+		return newList;
+		
+	}
+	
+	public void nextGeneration() {
+		
 		// increment the generation;
-		Generation++; 
-
+		generation++;
 
 		// check who can die
-		for  (int i = 0; i < Genomes.Count; i++)
-		{
-			if  (((Genome)Genomes[i]).CanDie(kDeathFitness))
-			{
-				Genomes.RemoveAt(i);
+		for (int i = 0; i < genomes.size(); i++) {
+			if (((Genome) genomes.get(i)).canDie(kDeathFitness)) {
+				genomes.remove(i);
 				i--;
 			}
 		}
 
-
 		// determine who can reproduce
-		GenomeReproducers.Clear();
-		GenomeResults.Clear();
-		for  (int i = 0; i < Genomes.Count; i++)
-		{
-			if (((Genome)Genomes[i]).CanReproduce(kReproductionFitness))
-			{
-				GenomeReproducers.Add(Genomes[i]);			
+		genomeReproducers.clear();
+		genomeResults.clear();
+		
+		for (int i = 0; i < genomes.size(); i++) {
+			if (((Genome) genomes.get(i)).canReproduce(kReproductionFitness)) {
+				genomeReproducers.add(genomes.get(i));
 			}
 		}
-		
-		// do the crossover of the genes and add them to the population
-		DoCrossover(GenomeReproducers);
 
-		Genomes = (ArrayList)GenomeResults.Clone();
+		// do the crossover of the genes and add them to the population
+		doCrossover(genomeReproducers);
+
+		genomes = (List) clone(genomeResults);
 
 		// mutate a few genes in the new population
-		for  (int i = 0; i < Genomes.Count; i++)
-		{
-			Mutate((Genome)Genomes[i]);
+		for (int i = 0; i < genomes.size(); i++) {
+			mutate((Genome) genomes.get(i));
 		}
 
 		// calculate fitness of all the genes
-		for  (int i = 0; i < Genomes.Count; i++)
-		{
-			((Genome)Genomes[i]).CalculateFitness();
+		for (int i = 0; i < genomes.size(); i++) {
+			((Genome) genomes.get(i)).calculateFitness();
 		}
-
-
-//		Genomes.Sort();
 
 		// kill all the genes above the population limit
-		if (Genomes.Count > kPopulationLimit)
-			Genomes.RemoveRange(kPopulationLimit, Genomes.Count - kPopulationLimit);
-		
-		CurrentPopulation = Genomes.Count;
-		
+		if (genomes.size() > kPopulationLimit) {
+			removeRange(genomes, kPopulationLimit, genomes.size() - kPopulationLimit);
+		}
+
+		currentPopulation = genomes.size();
 	}
 
-	public void CalculateFitnessForAll(ArrayList genes)
-	{
-		foreach(EquationGenome lg in genes)
-		{
-		  lg.CalculateFitness();
+	public void calculateFitnessForAll(final List<EquationGenome> genes) {
+	
+		for (EquationGenome lg : genes) {
+		
+		  lg.calculateFitness();
+		  
 		}
 	}
 
-	public void DoCrossover(ArrayList genes)
-	{
-		ArrayList GeneMoms = new ArrayList();
-		ArrayList GeneDads = new ArrayList();
+	public void doCrossover(final List genes) {
+		
+		final List geneMoms = new ArrayList();
+		final List geneDads = new ArrayList();
 
-		for (int i = 0; i < genes.Count; i++)
-		{
+		for (int i = 0; i < genes.size(); i++) {
+			
 			// randomly pick the moms and dad's
-			if (EquationGenome.TheSeed.Next(100) % 2 > 0)
-			{
-				GeneMoms.Add(genes[i]);
-			}
-			else
-			{
-				GeneDads.Add(genes[i]);
+			if (EquationGenome.TheSeed.nextInt(100) % 2 > 0) {
+				geneMoms.add(genes.get(i));
+			} else {
+				geneDads.add(genes.get(i));
 			}
 		}
 
-		//  now make them equal
-		if (GeneMoms.Count > GeneDads.Count)
-		{
-			while (GeneMoms.Count > GeneDads.Count)
-			{
-				GeneDads.Add(GeneMoms[GeneMoms.Count - 1]);
-				GeneMoms.RemoveAt(GeneMoms.Count - 1);
+		// now make them equal
+		if (geneMoms.size() > geneDads.size()) {
+			
+			while (geneMoms.size() > geneDads.size()) {
+				geneDads.add(geneMoms.get(geneMoms.size() - 1));
+				geneMoms.remove(geneMoms.size() - 1);
 			}
 
-			if (GeneDads.Count > GeneMoms.Count)
-			{
-				GeneDads.RemoveAt(GeneDads.Count - 1); // make sure they are equal
+			if (geneDads.size() > geneMoms.size()) {
+				geneDads.remove(geneDads.size() - 1); // make sure they are
+														// equal
 			}
 
-		}
-		else
-		{
-			while (GeneDads.Count > GeneMoms.Count)
-			{
-				GeneMoms.Add(GeneDads[GeneDads.Count - 1]);
-				GeneDads.RemoveAt(GeneDads.Count - 1);
+		} else {
+			
+			while (geneDads.size() > geneMoms.size()) {
+				geneMoms.add(geneDads.get(geneDads.size() - 1));
+				geneDads.remove(geneDads.size() - 1);
 			}
 
-			if (GeneMoms.Count > GeneDads.Count)
-			{
-				GeneMoms.RemoveAt(GeneMoms.Count - 1); // make sure they are equal
+			if (geneMoms.size() > geneDads.size()) {
+				geneMoms.remove(geneMoms.size() - 1); // make sure they are
+														// equal
 			}
 		}
 
 		// now cross them over and add them according to fitness
-		for (int i = 0; i < GeneDads.Count; i ++)
-		{
+		for (int i = 0; i < geneDads.size(); i++) {
+			
 			// pick best 2 from parent and children
 			EquationGenome babyGene1 = null;
-		    EquationGenome babyGene2 = null;
+			EquationGenome babyGene2 = null;
 
-			int randomnum = EquationGenome.TheSeed.Next(100) % 3;
-			if (randomnum == 0)
-			{
-				babyGene1 = (EquationGenome)((EquationGenome)GeneDads[i]).Crossover((EquationGenome)GeneMoms[i]);
-				babyGene2 = (EquationGenome)((EquationGenome)GeneMoms[i]).Crossover((EquationGenome)GeneDads[i]);
-			}
-			else if (randomnum == 1)
-			{
-				babyGene1 = (EquationGenome)((EquationGenome)GeneDads[i]).Crossover2Point((EquationGenome)GeneMoms[i]);
-				babyGene2 = (EquationGenome)((EquationGenome)GeneMoms[i]).Crossover2Point((EquationGenome)GeneDads[i]);
-			}
-			else
-			{
-				babyGene1 = (EquationGenome)((EquationGenome)GeneDads[i]).UniformCrossover((EquationGenome)GeneMoms[i]);
-				babyGene2 = (EquationGenome)((EquationGenome)GeneMoms[i]).UniformCrossover((EquationGenome)GeneDads[i]);
-			}
-		
-			GenomeFamily.Clear();
-			GenomeFamily.Add(GeneDads[i]);
-			GenomeFamily.Add(GeneMoms[i]);
-			GenomeFamily.Add(babyGene1);
-			GenomeFamily.Add(babyGene2);
-
-
-			CalculateFitnessForAll(GenomeFamily);
-
-			for (int j = 0; j < 4; j++)
-			{
-				CheckForUndefinedFitness((Genome)GenomeFamily[j]);
+			int randomnum = EquationGenome.TheSeed.nextInt(100) % 3;
+			
+			if (randomnum == 0) {
+				
+				babyGene1 = (EquationGenome) ((EquationGenome) geneDads.get(i)).crossover((EquationGenome) geneMoms.get(i));
+				babyGene2 = (EquationGenome) ((EquationGenome) geneMoms.get(i)).crossover((EquationGenome) geneDads.get(i));
+				
+			} else if (randomnum == 1) {
+				
+				babyGene1 = (EquationGenome) ((EquationGenome) geneDads.get(i)).crossover2Point((EquationGenome) geneMoms.get(i));
+				babyGene2 = (EquationGenome) ((EquationGenome) geneMoms.get(i)).crossover2Point((EquationGenome) geneDads.get(i));
+			} else {
+				
+				babyGene1 = (EquationGenome) ((EquationGenome) geneDads.get(i)).uniformCrossover((EquationGenome) geneMoms.get(i));
+				babyGene2 = (EquationGenome) ((EquationGenome) geneMoms.get(i)).uniformCrossover((EquationGenome) geneDads.get(i));
+				
 			}
 
-			GenomeFamily.Sort();
+			genomeFamily.clear();
+			genomeFamily.add(geneDads.get(i));
+			genomeFamily.add(geneMoms.get(i));
+			genomeFamily.add(babyGene1);
+			genomeFamily.add(babyGene2);
 
+			calculateFitnessForAll(genomeFamily);
 
-			if (Best2 == true)
-			{
+			for (int j = 0; j < 4; j++) {
+				
+				checkForUndefinedFitness((Genome) genomeFamily.get(j));
+				
+			}
+
+			Collections.sort(genomeFamily);
+			
+			if (best2 == true) {
+				
 				// if Best2 is true, add top fitness genes
-				GenomeResults.Add(GenomeFamily[0]);					
-				GenomeResults.Add(GenomeFamily[1]);					
+				genomeResults.add(genomeFamily.get(0));
+				genomeResults.add(genomeFamily.get(1));
 
-			}
-			else
-			{
-				GenomeResults.Add(babyGene1);
-				GenomeResults.Add(babyGene2);
+			} else {
+				
+				genomeResults.add(babyGene1);
+				genomeResults.add(babyGene2);
 			}
 
 			// if population shrunk, you can add rest of genes
-			if (Population.kPopulationLimit < Genomes.Count)
-			{
-				GenomeResults.Add(GenomeFamily[3]);					
-				GenomeResults.Add(GenomeFamily[4]);					
+			if (Population.kPopulationLimit < genomes.size()) {
+				
+				genomeResults.add(genomeFamily.get(3));
+				genomeResults.add(genomeFamily.get(4));
 			}
 		}
 
 	}
 
-	public void CheckForUndefinedFitness(Genome g)
-	{
-		if (double.IsNaN(g.CurrentFitness))
-			g.CurrentFitness = 0.01f;
+	public void checkForUndefinedFitness(final Genome g) {
+	
+		if (Double.isNaN(g.currentFitness))
+			g.currentFitness = 0.01f;
 	}
 
-
-	public void WriteNextGeneration()
-	{
+	public void writeNextGeneration() {
+		
 		// just write the top 20
-		Console.WriteLine("Generation {0}\n", Generation);
-		for  (int i = 0; i <  CurrentPopulation ; i++)
-		{
-			Console.WriteLine(((Genome)Genomes[i]).ToString());
+		System.out.println("generation " + generation);
+		for (int i = 0; i < currentPopulation; i++) {
+			System.out.println(((Genome) genomes.get(i)).toString());
 		}
 
-		Console.WriteLine("Generation #{0}, Hit the enter key to continue...\n", Generation);
-		Console.ReadLine();
+		System.out.println("generation #" + generation + ", Hit the enter key to continue...\n");
+		try {
+			System.in.read();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
-
-	public bool Converged()
-	{
+	
+	public boolean converged() {
+	
 		int histogram = 0;
-		for (int i=1; i < Scores.Count; i++)
-		{
-			if ((int)(((double)Scores[i])*10000) == (int)(((double)Scores[i-1])*10000))
-			{
+		for (int i=1; i < scores.size(); i++) {
+		
+			final Double ad = (Double) scores.get(i);
+			final Double bd = (Double) scores.get(i-1);
+			final double a = ad.doubleValue();
+			final double b = bd.doubleValue();
+			if ((int)(a * 10000) == (int)(b *10000)) {
+			
 				histogram++;
-			}
-			else
-			{
+				
+			}  else {
+			
 			  histogram = 0;
+			  
 			}
 		}
 
@@ -246,14 +278,15 @@ public class Population {
 		return false;
 	}
 
-	public void WriteNextGenerationBest()
-	{
-		Genomes.Sort();
-		if (Genomes.Count > 0)
-		{
-			Console.WriteLine(((Genome)Genomes[0]).ToString());
-			Scores.Add(((EquationGenome)Genomes[0]).CurrentFitness);  // add top score;
+	public void writeNextGenerationBest() {
+	
+		Collections.sort(genomes);
+		if (genomes.size() > 0) {
+		
+			System.out.println(((Genome) genomes.get(0)));
+			scores.add(((EquationGenome) genomes.get(0)).currentFitness);  // add top score;
 		}
-	}
-
-}
+	
+	} // End of the Method
+	
+} // End of Class
