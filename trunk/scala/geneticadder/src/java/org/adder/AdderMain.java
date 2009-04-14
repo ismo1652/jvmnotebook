ac1b949b2c5659d58fbf8c86bb07da29b24bc819
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2009-04-14 02:28:00 +0000

    able to get compilable code

--- trunk/scala/geneticadder/src/java/org/adder/AdderMain.java
+++ trunk/scala/geneticadder/src/java/org/adder/AdderMain.java
@@ -6,6 +6,19 @@ public class AdderMain {
 		
 		System.out.println("Running");
 		
+		final Population p = new Population();
+		p.writeNextGeneration();
+
+		int count = 0;
+		while (p.converged() == false) {
+			
+			p.nextGeneration();
+			if (count % 50 == 0) {
+				p.writeNextGeneration();
+			}
+			count++;
+		}
+		
 		System.out.println("Done");
 		
 	}
--- trunk/scala/geneticadder/src/java/org/adder/Genome.java
+++ trunk/scala/geneticadder/src/java/org/adder/Genome.java
@@ -1,9 +1,22 @@
 package org.adder;
 
-public class Genome {
+public abstract class Genome implements Comparable {
 
-	public long Length;
-	public int  CrossoverPoint;
-	public int  MutationIndex;
-	public double CurrentFitness = 0.0f;
-}
+	protected long   length;
+	protected int    crossoverPoint;
+	protected int    mutationIndex;
+	protected double currentFitness = 0.0f;
+	
+	abstract public void     initialize();
+	abstract public void     mutate();
+	abstract public Genome   uniformCrossover(final Genome g);
+	abstract public Genome   crossover2Point(final Genome g);
+	abstract public Genome   crossover(final Genome g);
+	abstract public Object   generateGeneValue(final int min, final int max, final int position);
+	abstract public void     setCrossoverPoint(final int crossoverPoint);
+	abstract public double   calculateFitness();
+	abstract public boolean  canReproduce(final double fitness);
+	abstract public boolean  canDie(final double fitness);	
+	abstract public void	 copyGeneInfo(final Genome g);
+			
+} // End of Class
