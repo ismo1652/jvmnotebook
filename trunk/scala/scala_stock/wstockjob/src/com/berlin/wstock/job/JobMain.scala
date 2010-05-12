102500d8d0230471b7d485d5821cc2ed7d0845fe
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2010-05-12 13:38:13 +0000

    adding stock code

---
+++ trunk/scala/scala_stock/wstockjob/src/com/berlin/wstock/job/JobMain.scala
@@ -0,0 +1,17 @@
+package com.berlin.wstock.job
+
+import scala.Array { Arr }
+import java.sql._
+import javax.sql.DataSource
+import org.apache.derby.drda.NetworkServerControl
+import java.util.Properties
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+object JobMain {	
+	
+	def main(args : Array[String]) : Unit = {
+		println("running job")
+	}
+  
+} // End of the Application //
