585b0744007510c6d0a9235ffb2922fbdcd89ba4
berlin.brown <berlin.brown@eafa03bc-2f3d-0410-b8ac-e166fe2dbcef>
2010-11-08 06:22:08 +0000

    Adding initial code

---
+++ trunk/gwt/game/octanewars/src/org/berlin/octanewars/game/model/Attributes.java
@@ -0,0 +1,183 @@
+/**
+ * Copyright (c) 2006-2010 Berlin Brown and botnode.com  All Rights Reserved
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * * Neither the name of the Botnode.com (Berlin Brown) nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Character.java
+ * Nov 7, 2010
+ * bbrown
+ * Contact: Berlin Brown <berlin dot brown at gmail.com>
+ */
+package org.berlin.octanewars.game.model;
+
+import java.io.Serializable;
+
+/**
+ * @author bbrown
+ *
+ */
+public class Attributes implements Serializable {
+
+    /**
+     * Serial version id.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Measure of how strong character is.
+     */
+    private int strength;
+    
+    /**
+     * Number of hit points.
+     */
+    private int hitPoints;
+    
+    /**
+     * Stamina.
+     */
+    private int constitution;
+    
+    /**
+     * Skill in magic.
+     */
+    private int intelligence;
+    
+    /**
+     * Dexterity.
+     */
+    private int speed;
+    
+    
+    private int defense; 
+    
+    /**
+     * Effects critical hits.
+     */
+    private int luck;
+
+    /**
+     * @return the strength
+     */
+    public int getStrength() {
+        return strength;
+    }
+
+    /**
+     * @param strength the strength to set
+     */
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    /**
+     * @return the hitPoints
+     */
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    /**
+     * @param hitPoints the hitPoints to set
+     */
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    /**
+     * @return the constitution
+     */
+    public int getConstitution() {
+        return constitution;
+    }
+
+    /**
+     * @param constitution the constitution to set
+     */
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    /**
+     * @return the intelligence
+     */
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    /**
+     * @param intelligence the intelligence to set
+     */
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    /**
+     * @return the speed
+     */
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * @param speed the speed to set
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * @return the defense
+     */
+    public int getDefense() {
+        return defense;
+    }
+
+    /**
+     * @param defense the defense to set
+     */
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    /**
+     * @return the luck
+     */
+    public int getLuck() {
+        return luck;
+    }
+
+    /**
+     * @param luck the luck to set
+     */
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+    
+} // End of the Class //
